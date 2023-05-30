cb93d91db9200eb0d8fcb998404ead386f540167
hw7319 <hw7319@naver.com>
2023-05-30 18:03:35 +0900

    반응속도

--- 4.반응속도체크/ResponseCheck.jsx
+++ 4.반응속도체크/ResponseCheck.jsx
@@ -5,6 +5,7 @@ class ResponseCheck extends Component {
     message: "클릭해서 시작하세요.",
     result: [],
   };
+  timeout;
   onClickScreen = () => {
     const { state, message, result } = this.state;
     if (state === "waiting") {
@@ -12,13 +13,27 @@ class ResponseCheck extends Component {
         state: "ready",
         message: "초록색이 되면 클릭하세요",
       });
-      setTimeout(() => {
+      this.timeout = setTimeout(() => {
         this.setState({
           state: "now",
           message: "지금 클릭",
         });
-      }, Math.floor(Math.random() * 1000) + 2000);
+      }, Math.floor(Math.random() * 1000) + 2000); // 2초 ~ 3초 랜덤
     } else if (state === "ready") {
+      clearTimeout(this.timeout);
+      this.setState({
+        state: "waiting",
+        message: "너무 성급하시군요! 초록색이 된 후에 클릭하세요!",
+      });
+
+      // 성급하게 클릭
+    } else if (state === "now") {
+      // 반응속도체크
+      this.setState({
+        state: "waiting",
+        message: "클릭해서 시작하세요!",
+        result: [],
+      });
     }
   };
   renderAverage = () => {
