de4078432e5afa8050fe59673bad9b37d24322ea
hw7319 <hw7319@naver.com>
2023-07-06 10:30:14 +0900

    숫자야구 ts 셋팅

---
+++ 3.숫자야구ts/TryClass.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import { TryInfo } from "./types";
+
+class Try extends React.Component<{ tryInfo: TryInfo }> {
+  render() {
+    const { tryInfo } = this.props;
+    return (
+      <li>
+        <div>{tryInfo.try}</div>
+        <div>{tryInfo.result}</div>
+      </li>
+    );
+  }
+}
